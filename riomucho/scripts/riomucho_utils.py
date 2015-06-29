ca7cf998223b29a7f7691c69b1471618722d5148
dnomadb <damon@mapbox.com>
2015-06-29 15:36:59 -0700

    adding better shapemaking + assertion

--- riomucho/scripts/riomucho_utils.py
+++ riomucho/scripts/riomucho_utils.py
@@ -16,11 +16,12 @@ def testUtils():
     return "yo"
 
 def array_stack(arrays):
+    shapes = np.array([a.shape for a in arrays])
+    assert np.all(np.roll(shapes[:,1:], 1, axis=0) == shapes[:,1:]), 'yo'
+
     width = arrays[0].shape[-1]
     height = arrays[0].shape[-2]
 
     return np.array(
         [a for subarray in arrays for a in subarray]
-        ).reshape(sum(
-            [a.shape[0] for a in arrays]
-            ), height, width)
+        ).reshape(shapes[:,0].sum(), height, width)
