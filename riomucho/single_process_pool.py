7d8ca7e214a4edd4acc65b4380d6d71184d8c6c3
dnomadb <damon@mapbox.com>
2016-06-17 08:53:52 -0700

    single process worker

---
+++ riomucho/single_process_pool.py
@@ -0,0 +1,10 @@
+class MockTub:
+    def __init__(self, main_worker, args):
+        main_worker(*args)
+    def imap_unordered(self, reader_worker, iterable):
+        for i in iterable:
+            yield reader_worker(i)
+    def close(self):
+        pass
+    def join(self):
+        pass
