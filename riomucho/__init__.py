09e76dcc05d971e07b1fc3e126e3565dea6ceed5
Sean C. Gillies <sean@mapbox.com>
2018-07-10 14:31:57 -0600

    Prep for 1.0
    Remove unneeded/untested code, add change log, new requirements
    enumeration, remove empty setup.cfg.

--- riomucho/__init__.py
+++ riomucho/__init__.py
@@ -1,3 +1,6 @@
+"""TODO
+"""
+
 from __future__ import with_statement
 from multiprocessing import Pool
 import rasterio as rio
@@ -14,63 +17,47 @@ srcs = None
 
 
 def main_worker(inpaths, g_work_func, g_args):
-    """"""
+    """TODO"""
     global work_func
     global global_args
     global srcs
     work_func = g_work_func
     global_args = g_args
-    try:
-        srcs = [rio.open(i) for i in inpaths]
-    except:
-        return
+    srcs = [rio.open(i) for i in inpaths]
+
 
 def manualRead(args):
-    try:
-        window, ij = args
-        return work_func(srcs, window, ij, global_args), window
-    except Exception as e:
-        traceback.print_exc()
-        raise e
+    """TODO"""
+    window, ij = args
+    return work_func(srcs, window, ij, global_args), window
+
 
 def arrayRead(args):
+    """TODO"""
     window, ij = args
-    try:
-        return work_func(utils.array_stack(
-            [src.read(window=window) for src in srcs]),
-            window, ij, global_args), window
-    except Exception as e:
-        traceback.print_exc()
-        raise e
+    return work_func(utils.array_stack(
+        [src.read(window=window) for src in srcs]),
+        window, ij, global_args), window
+
 
 def simpleRead(args):
+    """TODO"""
     window, ij = args
-    try:
-        return work_func([src.read(window=window) for src in srcs], window, ij, global_args), window
-    except Exception as e:
-        traceback.print_exc()
-        raise e
+    return work_func([src.read(window=window) for src in srcs], window, ij, global_args), window
+
 
 class RioMucho:
+    """TODO
+    """
+
     def __init__(self, inpaths, outpath, run_function, **kwargs):
+        """TODO
+        """
         self.inpaths = inpaths
-        if not 'windows' in kwargs:
-            self.windows = utils.getWindows(inpaths[0])
-        else:
-            self.windows = kwargs['windows']
+        self.outpath = outpath
+        self.run_function = run_function
 
-        if not 'options' in kwargs:
-            self.options = utils.getOptions(inpaths[0])
-        else:
-            self.options = kwargs['options']
-
-        if not 'global_args' in kwargs:
-            self.global_args = {}
-        else:
-            self.global_args = kwargs['global_args']
-
-
-        if not 'mode' in kwargs or kwargs['mode'] == 'simple_read':
+        if 'mode' not in kwargs or kwargs['mode'] == 'simple_read':
             self.mode = 'simple_read'
         elif kwargs['mode'] == 'array_read':
             self.mode = 'array_read'
@@ -79,17 +66,29 @@ class RioMucho:
         else:
             raise ValueError('mode must be one of: ["simple_read", "manual_read", "array_read"]')
 
-        self.outpath = outpath
-        self.run_function = run_function
+        if 'windows' not in kwargs:
+            self.windows = utils.getWindows(inpaths[0])
+        else:
+            self.windows = kwargs['windows']
+
+        if 'options' not in kwargs:
+            self.options = utils.getOptions(inpaths[0])
+        else:
+            self.options = kwargs['options']
+
+        if 'global_args' not in kwargs:
+            self.global_args = {}
+        else:
+            self.global_args = kwargs['global_args']
 
     def __enter__(self):
         return self
+
     def __exit__(self, ext_t, ext_v, trace):
-        if ext_t:
-            traceback.print_exc()
+        pass
 
     def run(self, processes=4):
-
+        """TODO"""
         if processes == 1:
             self.pool = MockTub(main_worker, (self.inpaths, self.run_function, self.global_args))
         else:
@@ -104,7 +103,8 @@ class RioMucho:
         else:
             reader_worker = simpleRead
 
-        ## Open an output file, work through the function in parallel, and write out the data
+        # Open an output file, work through the function in parallel,
+        # and write out the data.
         with rio.open(self.outpath, 'w', **self.options) as dst:
             for data, window in self.pool.imap_unordered(reader_worker, self.windows):
                 dst.write(data, window=window)
--- setup.py
+++ setup.py
@@ -4,27 +4,28 @@ from setuptools import setup, find_packages
 
 
 # Get the long description from the relevant file
-with codecs_open('README.rst', encoding='utf-8') as f:
+with codecs_open("README.rst", encoding="utf-8") as f:
     long_description = f.read()
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(name='rio-mucho',
-      version='0.2.2',
-      description=u"Windowed multiprocessing wrapper for rasterio",
-      long_description=long_description,
-      classifiers=[],
-      keywords='',
-      author=u"Damon Burgett",
-      author_email='damon@mapbox.com',
-      url='https://github.com/mapbox/rio-mucho',
-      license='MIT',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=read('requirements.txt').splitlines(),
-      extras_require={
-          'test': ['pytest', 'pytest-cov', 'coveralls'],
-      }
-      )
+
+setup(
+    name="rio-mucho",
+    version="1.0dev1",
+    description=u"Windowed multiprocessing wrapper for rasterio",
+    long_description=long_description,
+    classifiers=[],
+    keywords="",
+    author=u"Damon Burgett",
+    author_email="damon@mapbox.com",
+    url="https://github.com/mapbox/rio-mucho",
+    license="MIT",
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["click", "numpy", "rasterio>=1.0rc4"],
+    extras_require={"test": ["pytest", "pytest-cov", "coveralls"]},
+)
