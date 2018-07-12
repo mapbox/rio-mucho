b60d1536d1bb52484d399194e058fd2f86cb28ba
Sean C. Gillies <sean@mapbox.com>
2018-07-12 11:30:58 -0600

    Reformated
    Resolves #20

--- riomucho/utils.py
+++ riomucho/utils.py
@@ -23,11 +23,13 @@ def array_stack(arrays):
     shapes = np.array([a.shape for a in arrays])
 
     if not np.all(np.roll(shapes[:, 1:], 1, axis=0) == shapes[:, 1:]):
-        raise ValueError('All input arrays must have the same height and width for this mode')
+        raise ValueError(
+            "All input arrays must have the same height and width for this mode"
+        )
 
     width = arrays[0].shape[-1]
     height = arrays[0].shape[-2]
 
-    return np.array(
-        [a for subarray in arrays for a in subarray]
-    ).reshape(shapes[:, 0].sum(), height, width)
+    return np.array([a for subarray in arrays for a in subarray]).reshape(
+        shapes[:, 0].sum(), height, width
+    )
--- tests/conftest.py
+++ tests/conftest.py
@@ -8,29 +8,36 @@ import pytest
 
 def makeTesting(output, size, windowsize, bands):
     """Construct test fixture"""
-    kwargs = {'count': bands,
-        'crs': {'init': u'epsg:3857'},
-        'dtype': 'uint8',
-        'driver': u'GTiff',
-        'transform': Affine(4.595839562240513, 0.0, -13550756.3744,
-            0.0, -4.595839562240513, 6315533.02503),
-        'height': size,
-        'width': size,
-        'compress': 'lzw',
-        'blockxsize': windowsize,
-        'blockysize': windowsize,
-        'tiled': True
-        }
+    kwargs = {
+        "count": bands,
+        "crs": {"init": u"epsg:3857"},
+        "dtype": "uint8",
+        "driver": u"GTiff",
+        "transform": Affine(
+            4.595839562240513,
+            0.0,
+            -13550756.3744,
+            0.0,
+            -4.595839562240513,
+            6315533.02503,
+        ),
+        "height": size,
+        "width": size,
+        "compress": "lzw",
+        "blockxsize": windowsize,
+        "blockysize": windowsize,
+        "tiled": True,
+    }
 
-    randArr = np.array([
-        (np.random.rand(size, size) * 255).astype(np.uint8) for i in range(bands)
-        ])
+    randArr = np.array(
+        [(np.random.rand(size, size) * 255).astype(np.uint8) for i in range(bands)]
+    )
 
-    with rasterio.open(output, 'w', **kwargs) as dst:
+    with rasterio.open(output, "w", **kwargs) as dst:
         dst.write(randArr)
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def test_1_tif(tmpdir_factory):
     """Source dataset number 1"""
     fn = tmpdir_factory.mktemp("data").join("test_1.tif")
@@ -38,7 +45,7 @@ def test_1_tif(tmpdir_factory):
     return fn
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def test_2_tif(tmpdir_factory):
     """Source dataset number 2"""
     fn = tmpdir_factory.mktemp("data").join("test_2.tif")
